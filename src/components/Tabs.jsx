a40c3251c2e361e68470a2b2ef2283b304926122
dawidzabrzenski <dawidzabrzenski00@gmail.com>
2025-04-17 09:46:11 +0200

    styling: changes made to the tabs component, seems to be fully finished

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,7 @@
 import { motion } from 'motion/react';
 import { useState } from 'react';
 
-function Tabs({ tabs, defaultTab, onTabChange}) {
+function Tabs({ tabs, defaultTab, onTabChange }) {
   const [activeTab, setActiveTab] = useState(defaultTab || tabs[0]);
 
   const handleTabClick = (tab) => {
@@ -12,23 +12,21 @@ function Tabs({ tabs, defaultTab, onTabChange}) {
   };
 
   return (
-    <div
-      className={`bg-background-gray/30 border-secondary/30 flex gap-6 rounded-lg border px-2 py-2`}
-    >
+    <div className="bg-background-gray/30 border-secondary/30 flex gap-6 rounded-lg border px-2 py-2">
       {tabs.map((tab) => (
         <button
           key={tab}
-          className={`relative rounded-lg px-10 py-2 text-[1.125rem] tracking-[-0.5px] transitions-all duration-200 ${
+          className={`group relative rounded-lg px-10 py-2 text-[1.125rem] tracking-[-0.5px] transition-all duration-200 ${
             activeTab === tab
-              ? 'text-primary font-bold'
-              : 'text-font-gray font-medium hover:text-primary'
+              ? 'text-primary font-bold focus:outline-none'
+              : 'text-font-gray font-medium hover:text-primary focus:text-primary focus:outline-none'
           }`}
           onClick={() => handleTabClick(tab)}
         >
           {activeTab === tab && (
             <motion.div
               layoutId="tab-background"
-              className="absolute inset-0 bg-white rounded-lg z-0 outline-secondary/30 outline"
+              className="pointer-events-none absolute inset-0 z-0 rounded-lg bg-white outline outline-secondary/30 transition-colors duration-200 group-active:bg-background-gray group-hover:bg-background-gray group-focus:bg-primary/20 group-active:outline-primary group-focus:outline-primary"
             />
           )}
           <span className="relative z-10">{tab}</span>
